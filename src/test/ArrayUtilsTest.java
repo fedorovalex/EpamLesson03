c839666163ac55dbaab1b0152b9d2ee0f64fdb69
fedorovalex <INOY310@yandex.ru>
2016-11-13 22:16:09 +0300

    Add the function of the filter and predicates.

--- src/test/ArrayUtilsTest.java
+++ src/test/ArrayUtilsTest.java
@@ -1,11 +1,14 @@
 package test;
 
 import org.junit.Test;
-import task.ArrayUtils;
+import task.utils.ArrayUtils;
+import task.utils.predicate.MoreHundred;
+import task.utils.predicate.Parity;
+import task.utils.predicate.Positive;
 
 import java.util.Arrays;
 
-import static task.ArrayUtils.*;
+import static task.utils.ArrayUtils.*;
 
 import static org.junit.Assert.*;
 
@@ -78,5 +81,26 @@ public class ArrayUtilsTest {
         int[] source = {13, 2, 5, 2, 4, 5, 7, 5};
         String expectation = "13; 2; 5; 2; 4; 5; 7; 5.";
         assertTrue(expectation.equals(writeInString(source)));
+    }
+
+    @Test
+    public void TestFilterOnPredicateMoreHundred() {
+        int[] source = {13, 2, 550, 2, 401, 5, 70, 5};
+        int[] expectation = {550, 401};
+        assertTrue(Arrays.equals(expectation, filter(source, new MoreHundred())));
+    }
+
+    @Test
+    public void TestFilterOnPredicateParity() {
+        int[] source = {13, 2, 5, 2, 4, 5, 8, 5};
+        int[] expectation = {2, 2, 4, 8};
+        assertTrue(Arrays.equals(expectation, filter(source, new Parity())));
+    }
+
+    @Test
+    public void TestFilterOnPredicatePositive() {
+        int[] source = {-13, -2, 5, 2, 4, -5, -8, 5, 0};
+        int[] expectation = {5, 2, 4, 5};
+        assertTrue(Arrays.equals(expectation, filter(source, new Positive())));
     }
 }
